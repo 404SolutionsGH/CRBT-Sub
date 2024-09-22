7e3321cfde93ac7e4f3d2796f63feb3c35441635
herberth arthur <herbertharthur80@gmail.com>
2024-09-22 10:41:03 +0000

    Code Refactor: 1.Removed the try catch block in the middleware for verifying tokens

--- build/interface/middlewares/verifyJwt.js
+++ build/interface/middlewares/verifyJwt.js
@@ -5,26 +5,21 @@ const jwt_1 = require("../../libs/jwt");
 const AppError_1 = require("../../domain/entities/AppError");
 const verifyJwt = (req, res, next) => {
     console.log("Jwt verification began....");
-    try {
-        if (req.headers !== undefined && req.headers.authorization !== undefined) {
-            if (!req.headers.authorization.startsWith("Bearer ")) {
-                res.status(400);
-                throw new AppError_1.AppError("Bad Request Bearer schema not found in Header", 400);
-            }
-            const jwtData = (0, jwt_1.verifyToken)(req.headers.authorization.split(" ")[1]);
-            if (!jwtData.userId) {
-                throw new AppError_1.AppError("Token has expired or is not valid", 401);
-            }
-            console.log("Jwt token Verified");
-            req.body.id = jwtData.userId;
+    if (req.headers !== undefined && req.headers.authorization !== undefined) {
+        if (!req.headers.authorization.startsWith("Bearer ")) {
+            res.status(400);
+            throw new AppError_1.AppError("Bad Request Bearer schema not found in Header", 400);
         }
-        else {
-            throw new AppError_1.AppError("Authorization Header not defined", 400);
+        const jwtData = (0, jwt_1.verifyToken)(req.headers.authorization.split(" ")[1]);
+        if (!jwtData.userId) {
+            throw new AppError_1.AppError("Token has expired or is not valid", 401);
         }
-        next();
+        console.log("Jwt token Verified");
+        req.body.id = jwtData.userId;
     }
-    catch (error) {
-        throw new AppError_1.AppError(`${error.message}`, 500);
+    else {
+        throw new AppError_1.AppError("Authorization Header not defined", 400);
     }
+    next();
 };
 exports.verifyJwt = verifyJwt;
