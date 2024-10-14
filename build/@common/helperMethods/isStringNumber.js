2d72db7985bdca7b3724f74c9999452788dc2ede
hommym <herbertharthur80@gmail.com>
2024-10-14 20:46:44 +0100

    Refactored code in the isStringContentNumber method

--- build/@common/helperMethods/isStringNumber.js
+++ build/@common/helperMethods/isStringNumber.js
@@ -6,10 +6,10 @@ const isStringContentNumber = (content, variableName) => {
     const alphaReg = /[a-zA-Z]/;
     const decimalReg = /\./;
     if (alphaReg.test(content)) {
-        throw new AppError_1.AppError(`Item ${variableName} must be an integer not a character or alphanumeric characters`, 400);
+        throw new AppError_1.AppError(`Value for ${variableName} must be an integer not a character or alphanumeric characters`, 400);
     }
     else if (decimalReg.test(content)) {
-        throw new AppError_1.AppError(`Item ${variableName} must be an integer not decimal`, 400);
+        throw new AppError_1.AppError(`Value for ${variableName} must be an integer not decimal`, 400);
     }
 };
 exports.isStringContentNumber = isStringContentNumber;
--- src/@common/helperMethods/isStringNumber.ts
+++ src/@common/helperMethods/isStringNumber.ts
@@ -4,8 +4,8 @@ export const isStringContentNumber = (content: string,variableName:string) => {
   const alphaReg = /[a-zA-Z]/;
   const decimalReg = /\./;
   if (alphaReg.test(content)) {
-    throw new AppError(`Item ${variableName} must be an integer not a character or alphanumeric characters`, 400);
+    throw new AppError(`Value for ${variableName} must be an integer not a character or alphanumeric characters`, 400);
   } else if (decimalReg.test(content)) {
-    throw new AppError(`Item ${variableName} must be an integer not decimal`, 400);
+    throw new AppError(`Value for ${variableName} must be an integer not decimal`, 400);
   }
 };
